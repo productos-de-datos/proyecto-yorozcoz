7af50e11b0475ef9368e0ef6692d9e14b61ec01a
yorozcoz <68067003+yorozcoz@users.noreply.github.com>
2022-07-01 20:57:50 -0500

    Avance 5

--- yeison.py
+++
@@ -1,8 +0,0 @@
-import os
-initial_count = 0
-dir = "data_lake/landing/"
-for path in os.listdir(dir):
-    if os.path.isfile(os.path.join(dir, path)):
-        initial_count += 1
-print(initial_count)
-
