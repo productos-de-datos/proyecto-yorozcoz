48fa268f4cc362f8e2774baed0d7761b2130a251
yorozcoz <68067003+yorozcoz@users.noreply.github.com>
2022-07-01 21:54:09 -0500

    Update grader.py

--- grader.py
+++ grader.py
@@ -136,7 +136,7 @@ def test_10():
 def test_11():
     """Modelo creado"""
     os.system("make train_daily_model")
-    assert os.path.isfile("modeles/precios-diarios.pkl") is True
+    assert os.path.isfile("src/models/precios-diarios.pkl") is True
 
 
 def test_12():
